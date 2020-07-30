7fba05170bcab7cb3a56ab3bce41df0ea3137dbc
Your NameSuraj <suraj.barailee@cotiviti.com>
2020-07-30 12:18:08 +0000

    Classes

---
+++ Classes/complex.py
@@ -0,0 +1,31 @@
+class Students:
+
+    def __init__(self,name,password):
+        self.name=name
+        self.__password=password
+    def passwordret(self):
+        return self.__password
+
+class Additional(Students):
+    def __init__(self,name,password,language):
+        super().__init__(name,password)
+        self.language=language
+
+class MoreData(Additional,Students):
+     def __init__(self,name,password,language,address):
+         Additional.__init__(self,name,password,language)
+         self.address=address
+
+data=Students('Hari','Hari123')
+print(data.name)
+print(data.passwordret())
+
+data1=Additional('Ram','Ram123','Python')
+print(data1.passwordret())
+print(data1.language)
+
+obj1 = MoreData('Suraj','Suraj123','Java','Kathmandu')
+print(obj1.address)
+print(obj1.name)
+
+    
---
+++ Classes/simple.py
@@ -0,0 +1,17 @@
+class MyClass:
+    classvariable=30
+    def __init__(self,name,age):
+        self.name = name
+        self.age = age
+
+    def details(self):
+        print('Hello My name is ' + self.name + 'And I am of age ' + self.age)
+
+o1=MyClass('GOD',10000000000000)
+
+o2=MyClass('GODDESS',231524568978)
+print(o1.name)
+del o1
+
+print(o2.age)
+print(o2.classvariable)
